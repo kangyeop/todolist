3231b9e9b4dd1031d2e89d03b623227c6c329aca
kangyeop <lky5697@naver.com>
2020-11-10 14:10:13 +0900

    feat: Add LocalStorage Save

--- script.js
+++ script.js
@@ -1,5 +1,68 @@
 document.querySelector("#create-button").addEventListener("click", createList);
 
+let doData = localStorage.getItem("doData")
+    ? localStorage.getItem("doData").split(",")
+    : [];
+
+let doneData = localStorage.getItem("doneData")
+    ? localStorage.getItem("doneData").split(",")
+    : [];
+
+doData.map((data) => {
+    const ul = document.querySelector("#list-do");
+    const unique = (Math.random() * 100).toFixed(0);
+
+    const li = document.createElement("li");
+
+    const check = document.createElement("input");
+    check.setAttribute("type", "checkbox");
+    check.setAttribute("class", "btn-chk");
+    check.setAttribute("id", `do-button-${unique}`);
+    check.addEventListener("click", moveDone);
+    li.appendChild(check);
+
+    const node = document.createTextNode(data);
+    li.appendChild(node);
+
+    const removeButton = document.createElement("button");
+    removeButton.value = "삭제";
+    removeButton.setAttribute("class", "rmv-btn");
+    removeButton.setAttribute("id", `remove-button-${unique}`);
+    removeButton.append(document.createTextNode("삭제"));
+    removeButton.addEventListener("click", removeList);
+    li.appendChild(removeButton);
+
+    ul.appendChild(li);
+});
+
+doneData.map((data) => {
+    const ul = document.querySelector("#list-done");
+    const unique = (Math.random() * 100).toFixed(0);
+
+    const li = document.createElement("li");
+
+    const check = document.createElement("input");
+    check.setAttribute("type", "checkbox");
+    check.setAttribute("class", "btn-chk");
+    check.setAttribute("checked", true);
+    check.setAttribute("id", `do-button-${unique}`);
+    check.addEventListener("click", moveDone);
+    li.appendChild(check);
+
+    const node = document.createTextNode(data);
+    li.appendChild(node);
+
+    const removeButton = document.createElement("button");
+    removeButton.value = "삭제";
+    removeButton.setAttribute("class", "rmv-btn");
+    removeButton.setAttribute("id", `remove-button-${unique}`);
+    removeButton.append(document.createTextNode("삭제"));
+    removeButton.addEventListener("click", removeList);
+    li.appendChild(removeButton);
+
+    ul.appendChild(li);
+});
+
 function createList() {
     const input = document.querySelector("#input-box");
 
@@ -8,15 +71,22 @@ function createList() {
         return;
     }
 
+    const index = doData.findIndex((element) => element === input.value);
+    if (index >= 0) {
+        alert("이미 등록된 내용입니다.");
+        return;
+    }
+
     const ul = document.querySelector("#list-do");
-    const count = ul.childElementCount;
 
     const li = document.createElement("li");
+
+    const unique = (Math.random() * 100).toFixed(0);
 
     const check = document.createElement("input");
     check.setAttribute("type", "checkbox");
     check.setAttribute("class", "btn-chk");
-    check.setAttribute("id", `do-button${count}`);
+    check.setAttribute("id", `do-button-${unique}`);
     check.addEventListener("click", moveDone);
     li.appendChild(check);
 
@@ -26,12 +96,15 @@ function createList() {
     const removeButton = document.createElement("button");
     removeButton.value = "삭제";
     removeButton.setAttribute("class", "rmv-btn");
-    removeButton.setAttribute("id", `remove-button${count}`);
+    removeButton.setAttribute("id", `remove-button-${unique}`);
     removeButton.append(document.createTextNode("삭제"));
     removeButton.addEventListener("click", removeList);
     li.appendChild(removeButton);
 
     ul.appendChild(li);
+
+    doData.push(input.value);
+    localStorage.setItem("doData", doData);
 
     input.value = "";
 }
@@ -39,6 +112,19 @@ function createList() {
 function removeList(e) {
     const id = e.target.id;
     const li = document.querySelector(`#${id}`).parentNode;
+    if (li.parentNode.id.split("-")[1] === "do") {
+        const index = doData.findIndex(
+            (element) => element === li.childNodes[1].nodeValue
+        );
+        doData.splice(index, 1);
+        localStorage.setItem("doData", doData);
+    } else {
+        const index = doneData.findIndex(
+            (element) => element === li.childNodes[1].nodeValue
+        );
+        doneData.splice(index, 1);
+        localStorage.setItem("doneData", doneData);
+    }
     li.parentNode.removeChild(li);
 }
 
@@ -46,28 +132,40 @@ function moveDone(e) {
     const id = e.target.id;
     const li = document.querySelector(`#${id}`).parentNode;
     const newLi = li;
+
     li.parentNode.removeChild(li);
     const ulDone = document.querySelector("#list-done");
     const ulDo = document.querySelector("#list-do");
+    const unique = (Math.random() * 100).toFixed(0);
     if (e.target.checked) {
-        const count = ulDone.childElementCount;
+        const index = doData.findIndex(
+            (element) => element === newLi.childNodes[1].nodeValue
+        );
+        doData.splice(index, 1);
 
         const rmvbtn = newLi.childNodes[2];
         const donebtn = newLi.childNodes[0];
-        rmvbtn.setAttribute("id", `done-remove-button${count}`);
-        donebtn.setAttribute("id", `done-button${count}`);
+        rmvbtn.setAttribute("id", `done-remove-button-${unique}`);
+        donebtn.setAttribute("id", `done-button-${unique}`);
 
         ulDone.appendChild(newLi);
+        doneData.push(newLi.childNodes[1].nodeValue);
     } else {
-        const count = ulDo.childElementCount;
+        const index = doneData.findIndex(
+            (element) => element === newLi.childNodes[1].nodeValue
+        );
+        doneData.splice(index, 1);
 
         const rmvbtn = newLi.childNodes[2];
         const donebtn = newLi.childNodes[0];
-        rmvbtn.setAttribute("id", `remove-button${count}`);
-        donebtn.setAttribute("id", `do-button${count}`);
+        rmvbtn.setAttribute("id", `remove-button-${unique}`);
+        donebtn.setAttribute("id", `do-button-${unique}`);
 
         ulDo.appendChild(newLi);
+        doData.push(newLi.childNodes[1].nodeValue);
     }
+    localStorage.setItem("doData", doData);
+    localStorage.setItem("doneData", doneData);
 }
 
 function enterkey() {
